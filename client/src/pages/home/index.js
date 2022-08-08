a12d743afbe02747e03f886ab6da56758cb4e04a
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2022-08-08 11:38:59 +0200

    ChatRoom route setup

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,13 +1,32 @@
 import styles from './styles.module.css';
+import { useNavigate } from 'react-router-dom';
 
-const Home = () => {
+const Home = ({ username, setUsername, room, setRoom, socket }) => {
+
+    const navigate = useNavigate();
+
+    const joinRoom = () => {
+        if (room !== '' && username !== '') {
+          socket.emit('join_room', { username, room });
+        }
+        navigate('/chat', { replace: true });
+      };  
+    
+
   return (
     <div className={styles.container}>
       <div className={styles.formContainer}>
         <h1>{`Chat-Rooms`}</h1>
-        <input className={styles.input} placeholder='Username...' />
+        <input 
+            className={styles.input} 
+            placeholder='Username...' 
+            onChange={(e) => setUsername(e.target.value)}
+        />
 
-        <select className={styles.input}>
+        <select 
+            className={styles.input}
+            onChange={(e) => setRoom(e.target.value)}
+        >
           <option>-- Select Room --</option>
           <option value='Room Phu Quoc'>Phu Quoc</option>
           <option value='Room Madrid'>Madrid</option>
@@ -15,7 +34,13 @@ const Home = () => {
           <option value='Room Bern'>Bern</option>
         </select>
 
-        <button className='btn btn-secondary' style={{ width: '100%' }}>Join Room</button>
+        <button 
+            className='btn btn-secondary' 
+            style={{ width: '100%' }}
+            onClick={joinRoom}
+        >
+            Join Room
+        </button>
       </div>
     </div>
   );
